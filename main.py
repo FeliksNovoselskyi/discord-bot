fe53d844f2b06c7e86e4071dc8556649c6b18434
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2025-03-25 20:44:04 +0200

    add venv, add requirements.txt, add on_ready() and on_message() handlers in discord.py

--- main.py
+++ main.py
@@ -3,3 +3,17 @@
 """
 
 from modules import discord, gpt
+
+# 
+def main():
+    """
+    Головна функція, що відповідає за запуск бота
+    """
+
+    # 
+    discord.bot_client.run(token = discord.TOKEN)
+
+# 
+if __name__ == "__main__":
+    # 
+    main()
--- modules/discord.py
+++ modules/discord.py
@@ -3,3 +3,48 @@
 
 У цьому модулі прописана логіка взаємодії бота із Discord-сервером
 """
+
+import discord
+import os
+import dotenv
+
+# 
+dotenv.load_dotenv()
+
+# 
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+# 
+intents = discord.Intents.default()
+
+# 
+intents.message_content = True
+
+# 
+bot_client = discord.Client(intents = intents)
+
+
+@bot_client.event
+async def on_ready():
+    """
+    Функція відпрацьовує, коли бот запущено
+    """
+
+    # 
+    print("Бот запущено")
+
+
+@bot_client.event
+async def on_message(message):
+    """
+    Функція-подія, що відпрацьовує, коли від користувачів надходить повідомлення
+    """
+
+    # 
+    if message.author != bot_client.user:
+        # 
+        message_content = message.content
+
+        # 
+        await message.channel.send(message_content)
+
