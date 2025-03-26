e6831ef35f473c1f264fdce030a2ef934777b509
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2025-03-26 20:51:02 +0200

    add chatgpt in the project

--- modules/discord.py
+++ modules/discord.py
@@ -8,19 +8,22 @@ import discord
 import os
 import dotenv
 
-# 
+from .gpt import get_response_from_chatgpt
+
+
+# завантажуемо вміст файлу .env
 dotenv.load_dotenv()
 
-# 
+# отримуемо токен нашого дискорд бот
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
-# 
+# об'єкт що задає доступ боту до наших подій дискорду 
 intents = discord.Intents.default()
 
-# 
+# Доступ боту до читання контенту повідомлень
 intents.message_content = True
 
-# 
+# Об'єкт боту
 bot_client = discord.Client(intents = intents)
 
 
@@ -30,7 +33,7 @@ async def on_ready():
     Функція відпрацьовує, коли бот запущено
     """
 
-    # 
+    # Повідомлення про запуск боту
     print("Бот запущено")
 
 
@@ -40,11 +43,14 @@ async def on_message(message):
     Функція-подія, що відпрацьовує, коли від користувачів надходить повідомлення
     """
 
-    # 
+    # Якщо автором повідомлення не є сам бот
     if message.author != bot_client.user:
-        # 
+        # Отримуємо контент повідомлення 
         message_content = message.content
 
         # 
-        await message.channel.send(message_content)
+        response = await get_response_from_chatgpt(message_content)
+
+        # Надсилаємо у канал повідомлення від імені бота
+        await message.channel.send(response)
 
--- modules/gpt.py
+++ modules/gpt.py
@@ -3,3 +3,36 @@
 
 У цьому модулі прописана логіка взаємодії бота із серверами OpenAI
 """
+
+import openai
+import dotenv
+import os
+
+# 
+dotenv.load_dotenv()
+
+# 
+OPENAI_SECRET_KEY = os.getenv("OPENAI_SECRET_KEY")
+
+# 
+client_openAI = openai.AsyncOpenAI(
+    api_key = OPENAI_SECRET_KEY
+)
+
+async def get_response_from_chatgpt(question):
+    """
+    Функція відповідає за формування запиту до серверів OpenAI,
+    з метою отримання відповіді, згенерованої на основі ChatGPT
+    """
+
+    # 
+    response = await client_openAI.chat.completions.create(
+        model = "gpt-4o-mini", #
+        messages = [{
+            "role": "user", #
+            "content": question #
+        }]
+    )
+
+    # 
+    return response.choices[0].message.content
